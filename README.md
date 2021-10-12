e84b45a03d8821128e9e4b47b2f117c49fc330f1
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-10-12 14:04:16 -0400

    outputing the value

---
+++ README.md
@@ -0,0 +1,5 @@
+## Project
+Project on for loops to calculate mean height
+
+## Technology used 
+Python
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
 
 total_height= 0
+number_of_students = 0
 for height in student_heights:
     total_height += height
-print(total_height)
+    number_of_students +=1
+print(total_height/number_of_students)
 
