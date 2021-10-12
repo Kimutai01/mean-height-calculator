c18c8307127bba54c4507c7005a33b4e71e4ee05
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-10-12 14:26:01 -0400

    starting the student scores

---
+++ high_score.py
@@ -0,0 +1,10 @@
+student_scores = input("Input a list of student scores").split()
+for n in range(0, len(student_scores)):
+    student_scores[n]= int(student_scores[n])
+
+for score in stu
+
+print(student_scores)
+
+print(max(student_scores))
+print(min(student_scores))
